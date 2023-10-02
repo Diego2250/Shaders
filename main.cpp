b8bdede85a629717c8327ec2bfe0a310d63991e8
Diego Morales <77738746+Diego2250@users.noreply.github.com>
2023-10-01 22:47:36 -0600

    Project set up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include <SDL.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+
