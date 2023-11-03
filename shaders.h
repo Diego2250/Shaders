31e01fb6e9c8e29977daeafe1ebbad282aa5a760
Diego Morales <77738746+Diego2250@users.noreply.github.com>
2023-11-02 20:30:45 -0600

    shaders

--- shaders.h
+++ shaders.h
@@ -1,15 +1,12 @@
 #pragma once
 #include "glm/geometric.hpp"
 #include "glm/glm.hpp"
-#include "FastNoise.h"
 #include "uniform.h"
 #include "fragment.h"
 #include "noise.h"
 #include "print.h"
 
-static int frame = 0;
-
-Vertex vertexShader(const Vertex& vertex, const Uniforms& uniforms) {
+Vertex vertexShader(const Vertex& vertex, const Uniform& uniforms) {
     // Apply transformations to the input vertex using the matrices from the uniforms
     glm::vec4 clipSpaceVertex = uniforms.projection * uniforms.view * uniforms.model * glm::vec4(vertex.position, 1.0f);
 
@@ -35,210 +32,117 @@ Vertex vertexShader(const Vertex& vertex, const Uniforms& uniforms) {
     };
 }
 
-Fragment fragmentShaderStripes(Fragment& fragment) {
-    // Define the base color for Mars
-
-    // Create some noise based on the fragment position
-    /* float noise = glm::fract(sin(glm::dot(glm::vec2(fragment.x, fragment.y), glm::vec2(12.9898f, 78.233f))) * 43758.5453f); */
-
-    /* float noise = glm::fract(sin(glm::dot(fragment.worldPos, glm::vec3(12.9898f, 78.233f, 56.789f))) * 43758.5453f); */
-    // Slightly adjust the color based on the noise
-    /* Color color = marsColor + Color(static_cast<int>(noise * 25), static_cast<int>(noise * 10), static_cast<int>(noise * 5)); */
-    /* glm::vec3 wc = fragment.worldPos * 1.0f + 0.5f; */
-    /* Color color = Color(wc.r, wc.g, wc.b); */
-
-    // Define a color for Jupiter. You may want to adjust this to better match Jupiter's appearance.
-    Color baseColor = Color(125, 67, 37);  // This color is a kind of dark orange
-
-    // Create stripes based on the fragment's Y position in world space. The frequency of the stripes
-    // can be controlled by scaling the Y coordinate.
-    // The number 10.0f determines the frequency of the stripes, adjust it to get the desired effect.
-    float stripePattern = glm::abs(glm::cos(fragment.originalPos.y * 20.0f));
-
-    // Interpolate between the base color and a darker version of the base color based on the stripe pattern.
-    // This will create dark stripes on the sphere.
-    Color stripeColor = baseColor * (0.5f + 0.5f * stripePattern);
-
-    // Apply lighting intensity
-    stripeColor = stripeColor * fragment.intensity;
-
-    // Set the fragment color
-    Color color = stripeColor;
-
-
-
-    // Define the direction to the center of the circle in world space
-    // Apply lighting intensity
-    color = color * fragment.intensity;
-
-    // Set the fragment color
-    fragment.color = color;
-
-    return fragment;
-}
-
-Fragment fragmentShaderNorthPole(Fragment& fragment) {
-    // Define a color for the ocean and the continent
-    glm::vec3 oceanColor = glm::vec3(0, 0, 128);  // Dark blue
-    glm::vec3 continentColor = glm::vec3(34, 139, 34);  // Forest green
-
-    // Create a simple "continent" in the northern hemisphere by making a region where the Y coordinate is
-    // above a certain value look different
-    float continentPattern = glm::smoothstep(0.4f, 1.0f, fragment.originalPos.y);
-
-    // Interpolate between the ocean color and the continent color based on the continent pattern
-    glm::vec3 c = glm::mix(oceanColor, continentColor, continentPattern);
-    Color color = Color(c.x, c.y, c.z);
-    // Apply lighting intensity
-    color = color * fragment.intensity;
-
-    // Set the fragment color
-    fragment.color = color;
-
-    return fragment;
-}
-
-Fragment fragmentShaderGreeN(Fragment& fragment) {
-    // Define the color for the ocean and the continent using integer values
-    Color oceanColor = Color(0, 0, 128);  // Dark blue
-    Color continentColor = Color(34, 139, 34);  // Forest green
-
-    // Create a simple "continent" using a spot on the side of the planet.
-    // The spot is defined as a region where the x coordinate is within a certain range.
-    float continentPattern = glm::smoothstep(-0.2f, 0.2f, fragment.originalPos.x);
-
-    // Interpolate between the ocean color and the continent color based on the continent pattern
-    Color c = oceanColor * (1.0f - continentPattern) + continentColor * continentPattern;
-
-    // Apply lighting intensity
-    c = c * fragment.intensity;
-
-    // Set the fragment color
-    fragment.color = c;
-
-    return fragment;
-}
-
-Fragment fragmentShaderEarth(Fragment& fragment) {
+// Shader para el planeta rocoso
+Fragment planetaRocoso(Fragment& fragment) {
     Color color;
 
-    glm::vec3 groundColor = glm::vec3(0.44f, 0.51f, 0.33f);
-    glm::vec3 oceanColor = glm::vec3(0.12f, 0.38f, 0.57f);
-    glm::vec3 cloudColor = glm::vec3(1.0f, 1.0f, 1.0f);
+    // Definir los colores del planeta
+    glm::vec3 rockColor = glm::vec3(0.7f, 0.7f, 0.7f);
+    glm::vec3 surfaceColor = glm::vec3(0.4f, 0.4f, 0.4f);
 
-    float x = fragment.originalPos.x;
-    float y = fragment.originalPos.y;
-    float z = fragment.originalPos.z;
-    /* glm::vec2 uv = glm::vec2(fragment.originalPos.x, fragment.originalPos.y); */
-    float radius = sqrt(x*x + y*y + z*z);
+    // Obtener las coordenadas UV
+    glm::vec2 uv = glm::vec2(fragment.originalPos.x, fragment.originalPos.y);
 
-    /* glm::vec2 uv = glm::vec2( */
-    /*     atan2(x, z), */
-    /*     acos(y/sqrt(x*x + y*y + z*z)) */
-    /* ); */
-
-    glm::vec3 uv = glm::vec3(
-            atan2(x, z),
-            acos(y / radius),
-            radius
-    );
-
-    glm::vec3 uv2 = glm::vec3(
-            atan2(x + 10, z),
-            acos(y / radius),
-            radius
-    );
-
-
+    // Generar ruido para simular la textura del planeta
     FastNoiseLite noiseGenerator;
-    noiseGenerator.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
-    /* noiseGenerator.SetRotationType3D(FastNoiseLite::RotationType3D_ImproveXYPlanes); */
-    /* noiseGenerator.DomainWarp(uv.x, uv.y, uv.z); */
+    noiseGenerator.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
 
-    float ox = 1200.0f;
-    float oy = 3000.0f;
-    float zoom = 100.0f;
+    float offsetX = 5000.0f;
+    float offsetY = 8000.0f;
+    float scale = 500.0f;
 
-    /* float noiseValue1 = noiseGenerator.GetNoise(uv.x * zoom, uv.y * zoom); */
-    /* float noiseValue2 = noiseGenerator.GetNoise(uv.y * zoom + 1000.0f, uv.x * zoom + 1000.0f); */
-    /* float noiseValue = (noiseValue1 + noiseValue2) * 0.5f; */
+    float noiseValue = noiseGenerator.GetNoise((uv.x + offsetX) * scale, (uv.y + offsetY) * scale);
+    noiseValue = (noiseValue + 1.0f) * 0.5f; // Mapear [-1, 1] a [0, 1]
 
-    float noiseValue1 = noiseGenerator.GetNoise(uv.x * zoom, uv.y * zoom, uv.z * zoom);
-    float noiseValue2 = noiseGenerator.GetNoise(uv2.x * zoom + ox, uv2.y * zoom, uv2.z * zoom + oy);
-    float noiseValue = (noiseValue1 + noiseValue2) * 0.5f;
+    // Combinar los colores del planeta con el ruido
+    glm::vec3 finalColor = glm::mix(rockColor, surfaceColor, noiseValue);
 
-
-
-    glm::vec3 tmpColor = (noiseValue < 0.2f) ? oceanColor : groundColor;
-
-    float oxc = 5500.0f;
-    float oyc = 6900.0f;
-    float zoomc = 300.0f;
-
-    float noiseValueC = noiseGenerator.GetNoise((uv.x + oxc) * zoomc, (uv.y + oyc) * zoomc);
-
-    if (noiseValueC > 0.5f) {
-        /* tmpColor = cloudColor; */
-    }
-
-
-    color = Color(tmpColor.x, tmpColor.y, tmpColor.z);
-
-
+    color = Color(finalColor.x, finalColor.y, finalColor.z);
 
     fragment.color = color * fragment.intensity;
 
     return fragment;
 }
 
-Fragment fragmentShader(Fragment& fragment) {
 
+// Shader para el planeta gaseoso
+Fragment giganteGaseoso(Fragment& fragment) {
     Color color;
 
-    glm::vec3 brightColor = glm::vec3(1.0f, 0.6f, 0.0f);
-    glm::vec3 darkColor = glm::vec3(0.8f, 0.2f, 0.0f);
+    // Definir los colores del gigante gaseoso
+    glm::vec3 mainColor = glm::vec3(0.5f, 0.5f, 0.5f);
+    glm::vec3 cloudColor = glm::vec3(0.8f, 0.8f, 0.8f);
 
-    float x = fragment.originalPos.x;
-    float y = fragment.originalPos.y;
-    float z = fragment.originalPos.z;
-    float radius = sqrt(x*x + y*y + z*z);
+    // Obtener las coordenadas UV
+    glm::vec2 uv = glm::vec2(fragment.originalPos.x * 2.0 - 1.0, fragment.originalPos.y * 2.0 - 1.0);
 
-    glm::vec3 uv = glm::vec3(
-            atan2(x, z),
-            acos(y/radius),
-            radius
-    );
+    // Frecuencia y amplitud de las ondas en el gigante gaseoso
+    float frequency = 10.0; // Ajustar la frecuencia de las líneas
+    float amplitude = 0.2;  // Ajustar la amplitud de las líneas
 
     FastNoiseLite noiseGenerator;
+    noiseGenerator.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
 
+    float offsetX = 10000.0f;
+    float offsetY = 10000.0f;
+    float scale = 900.0f;
 
-    noiseGenerator.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
-    noiseGenerator.SetFrequency(0.02 + (10 - abs((static_cast<int>(frame/10.0f) % (2 * 10)) - 10))/2000.0f);
+    // Generar el valor de ruido
+    float noiseValue = noiseGenerator.GetNoise((uv.x + offsetX) * scale, (uv.y + offsetY) * scale);
+    noiseValue = (noiseValue + 1.0f) * 0.5f; // Mapear [-1, 1] a [0, 1]
 
-    float zoom = 1000.0f;
+    // Calcular el valor sinusoide para crear líneas
+    float sinValue = glm::sin(uv.y * frequency) * amplitude;
 
+    // Combinar el color base con las líneas sinusoide
+    cloudColor = mainColor + glm::vec3(sinValue);
 
-    float lat = uv.y;  // Latitude
-    float lon = uv.x;  // Longitude
-
-    // Normalize the direction vector
-    glm::vec3 dir = glm::normalize(glm::vec3(sin(lat) * cos(lon), sin(lat) * sin(lon), cos(lat)));
-
-    float noiseValue1 = noiseGenerator.GetNoise(uv.x * zoom, uv.y * zoom, uv.z * zoom);
-    float noiseValue2 = noiseGenerator.GetNoise(uv.x * zoom + 1000.0f, uv.y * zoom + 1000.0f);
-    float noiseValue = (noiseValue1 + noiseValue2) * 0.5f;
-
-    float edgeFactorY = sin(dir.y);  // Value is 0 at the poles and 1 at the equator
-    float edgeFactorX = sin(dir.x);  // Value is 0 at leftmost and rightmost points, 1 at front and back
-    float edgeFactor = edgeFactorX * edgeFactorY;
-    float alpha = 1.0f - glm::mix(1.0f, 0.0f, edgeFactor);  // Alpha is 1 at the poles and 0 at the equator
-
-    glm::vec3 tmpColor = mix(brightColor, darkColor, noiseValue);
-
-    color = Color(tmpColor.x, tmpColor.y, tmpColor.z, alpha);
-
+    color = Color(cloudColor.x, cloudColor.y, cloudColor.z);
 
     fragment.color = color * fragment.intensity;
 
     return fragment;
 }
+
+// Shader para la estrella
+Fragment estrellaCreativa(Fragment& fragment) {
+    Color color;
+
+    // Genera colores aleatorios para la estrella
+    float r = static_cast<float>(rand()) / RAND_MAX;
+    float g = static_cast<float>(rand()) / RAND_MAX;
+    float b = static_cast<float>(rand()) / RAND_MAX;
+
+    // Añade un efecto de destello aleatorio
+    float intensity = 1.0 + (rand() % 5) / 10.0;
+
+    // Simula cambios de color sutiles con el tiempo
+    float time = static_cast<float>(rand()) / RAND_MAX;
+    r += sin(time);
+    g += cos(time);
+    b += sin(time * 0.5);
+
+    // Añade un efecto de parpadeo aleatorio
+    intensity *= 0.8 + (rand() % 4) / 10.0;
+
+    // Añade un efecto de brillo aleatorio
+    intensity *= 1.0 + (rand() % 2) / 10.0;
+
+    // Limita los valores de color y intensidad
+    r = std::min(1.0f, r);
+    g = std::min(1.0f, g);
+    b = std::min(1.0f, b);
+    intensity = std::max(1.0f, intensity);
+
+    // Asigna el color y la intensidad a la fragment
+    color = Color(r, g, b);
+
+    // Aumenta la intensidad para simular el brillo de la estrella
+    fragment.color = color * intensity;
+
+    return fragment;
+}
+
+
+
+
